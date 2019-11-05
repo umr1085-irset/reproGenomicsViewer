bc8abdc362bd43773b6bb66b33df70687284299f
tdarde <thomas.darde@scilicium.com>
2019-11-05 23:11:27 +0100

    Update about

--- sdap/static/js/studies.js
+++ sdap/static/js/studies.js
@@ -1,42 +1,3 @@
-function ValidationFunction(){
-  console.log("Validation");
-  if($('.step').find('input[type="checkbox"]:checked').length)
-    return true;
-  return false;
-}
-
-function get_db_studies(destroyFeedback) {
-    var checkedValues = $('input:checkbox:checked.checkItems').map(function() { return this.value; }).get();
-    $.ajax({
-      url: url_getdb_studies,
-      type: 'get',
-      data: {
-        'db_ids': checkedValues
-      },
-      dataType: 'json',
-      success: function (data) {
-        $("#table_studies").html(data['table_list']);
-        destroyFeedback(true);
-      }
-    });
-}
-
-function get_db_datasets(destroyFeedback) {
-  var checkedValues = $('input:checkbox:checked.study').map(function() { return this.value; }).get();
-  $.ajax({
-    url: url_get_datasets,
-    type: 'get',
-    data: {
-      'db_ids': checkedValues
-    },
-    dataType: 'json',
-    success: function (data) {
-      $("#table_analyse").html(data['table_list']);
-      destroyFeedback(true);
-    }
-  });
-}
-
 function displayDatasets(study) {
   var checkedValues = $('input:checkbox:checked.dataset').map(function() { return this.value; }).get();
   var checkedStudy = $('input:checkbox:checked.dataset').map(function() { return this.name; }).get();
@@ -67,6 +28,7 @@ $("#step1").click( function()
   $("#step2").click( function()
     {
       var checkedValues = $('input:checkbox:checked.study').map(function() { return this.value; }).get();
+      alert(checkedValues)
       $.ajax({
         url: url_get_datasets,
         type: 'get',
