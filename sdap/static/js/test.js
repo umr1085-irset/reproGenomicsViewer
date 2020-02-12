5d8e15c43e4ab1c90755be36f238773a20a48705
mboudet <mateo.boudet@gmail.com>
2020-02-12 13:01:39 +0000

    Support and test for multi modals

--- sdap/static/js/test.js
+++ sdap/static/js/test.js
@@ -1,35 +1,43 @@
 var current_container=""
+var current_stack_id=1;
+var current_container_id=1;
 
 myLayout = new GoldenLayout({
     content:[{
         type: 'row',
         content:[{
+            title: 'New graph',
             type: 'component',
             componentName: 'testComponent',
-            componentState: { color: '#1D84BD' }
+            componentState: { text: ' Drag a new graph here, or use the cog button to select a graph.', modal_id: current_container_id }
         }]
     }]
 }, $('#layoutContainer'));
 
 myLayout.registerComponent( 'testComponent', function( container, state ){
-    container.getElement().css('background-color', state.color);
+    container.getElement().html('<p> ' + state.text + '</p>');
 });
 
 /// Callback for every created stack
 myLayout.on( 'stackCreated', function( stack ){
+    // Clone the default modal and append it with correct id, associated to graph
+    var clone = $($("#default_modal").html()).clone().prop('id', 'modal-' + current_stack_id );
+    $("#modal-wrapper").append(clone);
 
-    //HTML for the colorDropdown is stored in a template tag
-    var colorDropdown = $( $( 'template' ).html() );
+    // Edit the attr to the new modal id
+    var cogMenu = $( $( 'template' ).html() );
+    cogMenu.find("a").attr('data-target', "#modal-" + current_stack_id);
 
-    // Add the colorDropdown to the header
-    stack.header.controlsContainer.prepend( colorDropdown );
+    // Add the menu to the header
+    stack.header.controlsContainer.prepend( cogMenu );
+    current_stack_id += 1;
 
     var setActiveContainer = function(){
         current_container = stack.getActiveContentItem().container;
         // Update the state
     };
 
-    colorDropdown.click(function(){
+    cogMenu.click(function(){
         setActiveContainer();
     });
 
@@ -38,6 +46,9 @@ myLayout.on( 'stackCreated', function( stack ){
 myLayout.init();
 
 myLayout.on('componentCreated',function(component) {
+
+    component.container.extendState({ modal_id: current_container_id });
+    current_container_id += 1;
     component.container.on('resize',function() {
         if (component.container.getElement()[0].data){
             Plotly.relayout(component.container.getElement()[0], {
@@ -52,7 +63,7 @@ $(window).resize(function () {
     myLayout.updateSize();
 });
 
-var addMenuItem = function( title, color ) {
+var addMenuItem = function( title ) {
     var element = $( '<li>' + title + '</li>' );
     $( '#menuContainer' ).append( element );
 
@@ -60,20 +71,25 @@ var addMenuItem = function( title, color ) {
         title: title,
         type: 'component',
         componentName: 'testComponent',
-        componentState: { color: color }
+        componentState: { text: ' Drag a new graph here, or use the cog button to select a graph.'}
     };
 
     myLayout.createDragSource( element, newItemConfig );
 };
 
-addMenuItem( 'New graph', 'blue' );
+addMenuItem( 'New graph');
 
 
 $(function () {
 
     var set_graph = function(){
+        var url = $("#wrapper").attr("data-url");
+        console.log(current_container.getState().modal_id);
+        var current_form = $("#modal-" + current_container.getState().modal_id).find("form");
+        var selected_type = current_form.find(".visu-type").val();
+        var selected_class = current_form.find(".visu-class").val();
         $.ajax({
-            url: "http://openstack-192-168-100-69.genouest.org/studies/graph_data?document_id=152&mode=scatter&selected_class=Class:cluster",
+            url: url + "&mode="+ selected_type + "&selected_class=" + selected_class,
             type: 'get',
             dataType: 'json',
             success: function (data) {
@@ -85,12 +101,11 @@ $(function () {
                 chart.layout.height = myelem.offsetHeight;
                 current_container.setTitle("testtitle");
                 Plotly.newPlot(myelem, chart.data, chart.layout, chart.config);
-
             }
         });
 
 
     }
 
-    $(".test").on('click', set_graph);
+    $("#modal-wrapper").on('click', '.test', set_graph);
 });
