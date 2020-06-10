302b20622ff66104625d41ff4fc0335695c59644
mboudet <mateo.boudet@gmail.com>
2020-06-10 12:36:47 +0000

    Updated sync script

--- loading_data/sync_studies.sh
+++ loading_data/sync_studies.sh
@@ -2,15 +2,15 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-. "$DIR""/../.envs/.production/.django"
-. "$DIR""/../.envs/.production/.postgres"
+. "$DIR""/../.envs/.production/.django_prod"
+. "$DIR""/../.envs/.production/.postgres_prod"
 
 CELERY_BROKER_URL="${REDIS_URL}"
 DATABASE_URL="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}"
 STUDIES_FOLDER="/app/loading_data/rgv_data/studies"
+SYNC_FILE="/groups/irset/archives/web/RGV2/auto_sync_studies/metadata.csv"
 
-
-if [ -f "$ADD_FILE" ]; then
+if [ -f "$SYNC_FILE" ]; then
    echo "Metadata file found. Adding studies..."
    docker-compose -f "$DIR""/../production.yml" exec -e DATABASE_URL=$DATABASE_URL -e CELERY_BROKER_URL=$CELERY_BROKER_URL django python manage.py sync_studies "/app/loading_data/sync/metadata.csv" "/mnt/"
    rm "$ADD_FILE"
