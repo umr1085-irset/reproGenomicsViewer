98452d68f75e9f023933fc2b471c94dfffe559b0
root <root@web-irset.genouest.org>
2023-05-15 11:18:45 +0200

    sync prod

--- backups/backup_script.sh
+++ backups/backup_script.sh
@@ -22,6 +22,6 @@ tar -pzcf  "$LOCAL_BACKUP_DIR""archive-$DATE"".tar.gz" "$DIR""/../sdap/media/"
 rm "$DIR""/../sdap/media/DB_backup"
 
 cd $LOCAL_BACKUP_DIR
-ls -1t  | tail -n +4 | xargs rm -f
-echo "Rsync.."
-rsync -av --delete --exclude=".*" $LOCAL_BACKUP_DIR $REMOTE_BACKUP_DIR
+ls -1t  | tail -n +3 | xargs rm -f
+mv "$LOCAL_BACKUP_DIR""archive-$DATE"".tar.gz" $REMOTE_BACKUP_DIR
+ls -1t $REMOTE_BACKUP_DIR/*  | tail -n +3 | xargs rm -f
